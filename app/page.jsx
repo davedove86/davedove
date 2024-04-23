0d1a6a6f504c63615fef0b7c3ad95b28cf1ad448
Dave Dove <info@dovedesign.io>
2024-04-23 15:07:21 +0100

    main heading color on dark changed

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
       <div className="flex">
         <Image src={Profile} alt="Dave Dove" width={128} height={128} />
       </div>
-      <h1 className='text-2xl font-medium'>Dave Dove</h1>
+      <h1 className='text-2xl font-medium text-dark dark:text-white'>Dave Dove</h1>
       <p>Hi there, I am a software engineer who builds for the web with a design-oriented approach. I currently run my own Web Design Agency, <a href='https://www.dovedesign.io' target='blank'>Dove Design</a> which design and builds websites for businesses.</p>
     </section>
 
